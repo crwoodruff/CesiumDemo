0ee5085c18f09af71004f0dc3eac5ec80424cced
Woodruff <woodruffc1@leidos.com>
2017-09-13 12:34:14 -0500

    Connect Points with lines

--- CesiumDemo/src/com/cesiumdemo/gwt/client/CesiumDemo.java
+++ CesiumDemo/src/com/cesiumdemo/gwt/client/CesiumDemo.java
@@ -4,7 +4,9 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JOptionPane;
 
+import org.cesiumjs.cs.Cesium;
 import org.cesiumjs.cs.collections.EntityCollection;
 import org.cesiumjs.cs.collections.ImageryLayerCollection;
 import org.cesiumjs.cs.core.Cartesian2;
@@ -12,18 +14,23 @@ import org.cesiumjs.cs.core.Cartesian3;
 import org.cesiumjs.cs.core.Cartographic;
 import org.cesiumjs.cs.core.Color;
 import org.cesiumjs.cs.core.Globe;
+import org.cesiumjs.cs.core.JulianDate;
+import org.cesiumjs.cs.core.PickedObject;
 import org.cesiumjs.cs.core.PolygonHierarchy;
 import org.cesiumjs.cs.core.ScreenSpaceEventHandler;
 import org.cesiumjs.cs.core.enums.ScreenSpaceEventType;
 import org.cesiumjs.cs.core.events.MouseClickEvent;
 import org.cesiumjs.cs.core.events.MouseMoveEvent;
+import org.cesiumjs.cs.datasources.DataSource;
 import org.cesiumjs.cs.datasources.Entity;
 import org.cesiumjs.cs.datasources.graphics.EllipseGraphics;
 import org.cesiumjs.cs.datasources.graphics.LabelGraphics;
 import org.cesiumjs.cs.datasources.graphics.PointGraphics;
 import org.cesiumjs.cs.datasources.graphics.PolygonGraphics;
+import org.cesiumjs.cs.datasources.graphics.PolylineGraphics;
 import org.cesiumjs.cs.datasources.graphics.options.EllipseGraphicsOptions;
 import org.cesiumjs.cs.datasources.graphics.options.LabelGraphicsOptions;
+import org.cesiumjs.cs.datasources.graphics.options.PointGraphicsOptions;
 import org.cesiumjs.cs.datasources.graphics.options.PolygonGraphicsOptions;
 import org.cesiumjs.cs.datasources.properties.ColorMaterialProperty;
 import org.cesiumjs.cs.datasources.properties.ConstantPositionProperty;
@@ -60,6 +67,8 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 
+import javafx.scene.shape.Polyline;
+
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
@@ -87,6 +96,8 @@ public class CesiumDemo implements EntryPoint  {
 		  //customBaseLayerPicker(viewerOptions, myVPanel, globe);
 	      drawShapes();    
 	      drawCustomPolygon();
+	      highlightOnMouseOver();
+	      connectMarkers();
 	      showPositionOnMouseOver();
 	  }
 	  
@@ -115,6 +126,24 @@ public class CesiumDemo implements EntryPoint  {
 	      
 	  }	//	End of method customBaseLayerPicker	 */
 	  
+	  //Create PointGraphics Entity
+	  private Entity createNewPointEntity(Cartesian3 position, Color color, 
+			  Color outlineColor, Integer pixelSize) {
+		  Entity myEntity = new Entity();		  
+		  PointGraphicsOptions myPointOpt = new PointGraphicsOptions();
+		  		  
+		  myPointOpt.color = new ConstantProperty<>(color);
+		  myPointOpt.outlineColor = new ConstantProperty<>(outlineColor);
+		  myPointOpt.outlineWidth = new ConstantProperty<>(2);
+		  myPointOpt.pixelSize = new ConstantProperty<>(pixelSize);		  
+		  myEntity.position = new ConstantPositionProperty(position);
+		  
+		  PointGraphics myPoint = new PointGraphics(myPointOpt);
+		  myEntity.point = myPoint;
+		  
+		  return myEntity;
+	  }
+	  
 	  //Draw user-defined shapes
 	  private void drawShapes() {
 		  
@@ -122,30 +151,24 @@ public class CesiumDemo implements EntryPoint  {
 		  ConstantPositionProperty position = new ConstantPositionProperty();
 		  position.setValue(Cartesian3.fromDegrees(-77.0, 38.9));
 		  
-		  //Point Entity
-		  Entity pointEntity = new Entity();	  
-		  
-		  //Label  
+		  //Point Entities
+		  Entity pointEntity = createNewPointEntity(Cartesian3.fromDegrees(-77.0, 38.9), Color.DARKRED(), Color.AQUA(), 10);
+		  Entity pointEntity1 = createNewPointEntity(Cartesian3.fromDegrees(-79.08, 38.38), Color.PURPLE(), Color.BLUE(), 10);
+		  Entity pointEntity2 = createNewPointEntity(Cartesian3.fromDegrees(-79.2, 38.40), Color.PURPLE(), Color.BLUE(), 10);
+		  Entity pointEntity3 = createNewPointEntity(Cartesian3.fromDegrees(-79.15, 38.36), Color.PURPLE(), Color.BLUE(), 10);		  
+		  		  
+		  //Point Label  
 		  LabelGraphicsOptions lgOptions = new LabelGraphicsOptions();
 		  lgOptions.horizontalOrigin = new ConstantProperty<>(HorizontalOrigin.LEFT());
 		  lgOptions.verticalOrigin = new ConstantProperty<>(VerticalOrigin.TOP());
 		  lgOptions.eyeOffset = new ConstantProperty<>(new Cartesian3(5,5,0));
 		  pointEntity.label = new LabelGraphics(lgOptions);
-		  pointEntity.label.text = new ConstantProperty<>("Point Entity");
-		  
-		  //Point Options
-		  pointEntity.position = position;		  
-		  PointGraphics myPoint = new PointGraphics();
-		  myPoint.color = new ConstantProperty<>(Color.DARKRED());
-		  myPoint.pixelSize = new ConstantProperty<>(5);
-		  myPoint.outlineColor = new ConstantProperty<>(Color.AQUA());
-		  myPoint.outlineWidth = new ConstantProperty<>(2);
-		  pointEntity.point = myPoint;
+		  pointEntity.label.text = new ConstantProperty<>("Point Entity");  
 		  
 		  //Ellipse Entity
 		  Entity ellipseEntity = new Entity();
 		  
-		  //Label
+		  //Ellipse Label
 		  ellipseEntity.label = new LabelGraphics();
 		  ellipseEntity.label.text = new ConstantProperty<>("Ellipse Entity");
 		  ellipseEntity.label.horizontalOrigin = new ConstantProperty<>(HorizontalOrigin.LEFT());
@@ -161,7 +184,7 @@ public class CesiumDemo implements EntryPoint  {
 		  ellipseOpt.semiMinorAxis = new ConstantProperty<>(75000.0);	
 		  ellipseOpt.outline = new ConstantProperty<>(true);
 		  ellipseOpt.outlineColor = new ConstantProperty<>(Color.AQUA());
-		  ellipseOpt.outlineWidth = new ConstantProperty<>(20);
+		  ellipseOpt.outlineWidth = new ConstantProperty<>(30);
 		  
 		  EllipseGraphics ellipse = new EllipseGraphics(ellipseOpt);			  
 		  ellipse.height = new ConstantProperty<>(0);  
@@ -170,7 +193,7 @@ public class CesiumDemo implements EntryPoint  {
 		  //Polygon Entity
 		  Entity polygonEntity = new Entity();
 		  
-		  //Label		  
+		  //Polygon Label		  
 		  LabelGraphicsOptions lgPolyOptions = new LabelGraphicsOptions();
 		  lgPolyOptions.text = new ConstantProperty<>("Polygon Entity");
 		  lgPolyOptions.horizontalOrigin = new ConstantProperty<>(HorizontalOrigin.LEFT());
@@ -197,8 +220,11 @@ public class CesiumDemo implements EntryPoint  {
 		  myViewer.entities().add(ellipseEntity);
 		  myViewer.entities().add(polygonEntity);
 		  myViewer.entities().add(pointEntity);
-
-		  myViewer.selectedEntity = pointEntity;
+		  myViewer.entities().add(pointEntity1);
+		  myViewer.entities().add(pointEntity2);
+		  myViewer.entities().add(pointEntity3);
+		  
+		  myViewer.selectedEntity = pointEntity1;
 		  //myViewer.zoomTo(myViewer.selectedEntity);
 	  }	//End of method drawShapes
 	  
@@ -209,7 +235,7 @@ public class CesiumDemo implements EntryPoint  {
 		  		  
 		  List<Cartesian3> currentPolyVert = new ArrayList<Cartesian3>();	  
 		  Integer numPoly = 0;
-		  ScreenSpaceEventHandler.Listener<MouseClickEvent> myClickEvent = new ScreenSpaceEventHandler.Listener<MouseClickEvent>() {
+		  ScreenSpaceEventHandler.Listener<MouseClickEvent> drawPolygonHandler = new ScreenSpaceEventHandler.Listener<MouseClickEvent>() {
 			  @Override
 			  public void function(MouseClickEvent event) {
 				  Cartesian3 myPosition = getPosition(event);
@@ -222,23 +248,26 @@ public class CesiumDemo implements EntryPoint  {
 		  //Action Buttons
 		  Button btnDrawPolygon = new Button();
 		  btnDrawPolygon.setText("Draw Polygon");
+		  btnDrawPolygon.setWidth("120px");
 		  
-		  Button btnDone = new Button();
-		  btnDone.setText("Done");	  
+		  Button btnPolygonDone = new Button();
+		  btnPolygonDone.setText("Done");	
+		  btnPolygonDone.setWidth("60px");	  
 		  
-		  Button btnCancel = new Button();
-		  btnCancel.setText("Cancel");
+		  Button btnPolygonCancel = new Button();
+		  btnPolygonCancel.setText("Cancel");
+		  btnPolygonCancel.setWidth("60px");
 		  
 		  btnDrawPolygon.addClickHandler( new ClickHandler() {
 		      public void onClick(ClickEvent event) {
 		    	  
 		    	  //Add polygon click handler back to map
-		    	  myHandler.setInputAction(myClickEvent, ScreenSpaceEventType.LEFT_CLICK());
+		    	  myHandler.setInputAction(drawPolygonHandler, ScreenSpaceEventType.LEFT_CLICK());
 		    	  currentPolyVert.clear();
 		    	  
 		    	  //Add done and cancel buttons
-		    	  absPanel.add(btnDone, 180, 30);
-		    	  absPanel.add(btnCancel, 120, 30);		    	  
+		    	  absPanel.add(btnPolygonDone, 132, 35);
+		    	  absPanel.add(btnPolygonCancel, 194, 35);		    	  
 		   		    	  
 		    	  //Create new Entity polygon and add to map
 		    	  Entity polyEntity = new Entity();		
@@ -251,30 +280,170 @@ public class CesiumDemo implements EntryPoint  {
 				  polyEntity.polygon = myPolygon;				  
 		      }
 		    });
-		  absPanel.add(btnDrawPolygon, 10, 30);		  
+		  absPanel.add(btnDrawPolygon, 10, 35);		  
 		  
-		  btnDone.addClickHandler( new ClickHandler() {
+		  btnPolygonDone.addClickHandler( new ClickHandler() {
 			  @Override
 		      public void onClick(ClickEvent event) {
 				  myHandler.removeInputAction(ScreenSpaceEventType.LEFT_CLICK());
-				  absPanel.remove(btnDone);
-				  absPanel.remove(btnCancel);
+				  absPanel.remove(btnPolygonDone);
+				  absPanel.remove(btnPolygonCancel);
 		      }
 		    });
 		  
-		  btnCancel.addClickHandler( new ClickHandler() {
+		  btnPolygonCancel.addClickHandler( new ClickHandler() {
 			  @Override
 		      public void onClick(ClickEvent event) {
 				  myViewer.entities().remove(myViewer.entities().getById(currentPolyId));
 				  myHandler.removeInputAction(ScreenSpaceEventType.LEFT_CLICK());
-				  absPanel.remove(btnDone);
-				  absPanel.remove(btnCancel);
+				  absPanel.remove(btnPolygonDone);
+				  absPanel.remove(btnPolygonCancel);
 		      }
 		    });
 	  }	  
 	  
+	  //Draw lines between markers
+	  String currentLineId;
+	  private void connectMarkers() {
+	      
+		  //Initialize vars for Entity Select Handlers
+		  PointGraphics pickedPoint = new PointGraphics();
+		  List<Cartesian3> polylineVerts = new ArrayList<>();
+		  ScreenSpaceEventHandler pickEntityHandler = new ScreenSpaceEventHandler(myVPanel.getViewer().scene().canvas());
+		  ScreenSpaceEventHandler dynamicLineHandler = new ScreenSpaceEventHandler(myVPanel.getViewer().scene().canvas());
+
+	      //Add Action Buttons
+		  Button btnConnectPoints = new Button();
+		  btnConnectPoints.setText("Connect Points");
+		  btnConnectPoints.setWidth("120px");
+		  
+		  Button btnConnectDone = new Button();
+		  btnConnectDone.setText("Done");	  
+		  btnConnectDone.setWidth("60px");	  
+		  
+		  Button btnConnectCancel = new Button();
+		  btnConnectCancel.setText("Cancel");
+		  btnConnectCancel.setWidth("60px");	  
+		  Entity currentLineEntity = new Entity();
+		  btnConnectPoints.addClickHandler( new ClickHandler() {
+		      public void onClick(ClickEvent event) {
+				  absPanel.add(btnConnectDone, 132, 60);
+		    	  absPanel.add(btnConnectCancel, 194, 60);
+		    	  
+		    	  //Create new Polyline Entity
+			      Entity lineEntity = new Entity();
+				  PolylineGraphics myPolyLine = new PolylineGraphics();
+				  lineEntity.polyline = myPolyLine;
+				  currentLineId = lineEntity.id;
+				  myViewer.entities().add(lineEntity);
+				  
+		    	  //Add handler to Select an Entity on click
+		    	  pickEntityHandler.setInputAction(new ScreenSpaceEventHandler.Listener<MouseClickEvent>() {
+					  @Override
+					  public void function(MouseClickEvent event) {		
+						  Entity pickedEntity = new Entity();
+						  
+						  //Pick Entity on mouse click
+						  try {
+							  pickedObject = (PickedObject<Entity>) myVPanel.getViewer().scene().pick(event.position);						  	
+						  }
+						  catch(ClassCastException e) {
+							  pickedObject = null;
+						  }				  
+						  
+						  //If Cesium found an Entity with point, add it to the polyline 				  
+						  if (Cesium.defined(pickedObject)) {
+							  pickedEntity = pickedObject.id;
+							  if (Cesium.defined(pickedEntity.point)) {
+								  Cartesian3 pickedPosition = pickedEntity.position.getValue(JulianDate.now());
+								  polylineVerts.add(pickedPosition);
+								  myPolyLine.positions = new ConstantProperty<>(polylineVerts.toArray());
+								  myPolyLine.definitionChanged();
+							  }
+							  //if selected Entity does not contain a Point
+							  else {	
+								  Window.alert("Please select an existing point on the map");
+							  }
+						  }		
+						  //If no Entity selected
+						  else {
+							  Window.alert("Please select an existing point on the map");
+						  }
+					  }
+				  }, ScreenSpaceEventType.LEFT_CLICK());
+		    	  
+		    	  //Add handler to add point to Polyline on mouse move
+				  dynamicLineHandler.setInputAction(new ScreenSpaceEventHandler.Listener<MouseMoveEvent>() {
+					  @Override
+					  public void function(MouseMoveEvent event) {			  			  
+						  	  
+					  }
+				  }, ScreenSpaceEventType.MOUSE_MOVE());
+		      }		      
+		   });
+		  
+		  btnConnectDone.addClickHandler( new ClickHandler() {
+		      public void onClick(ClickEvent event) {
+				  absPanel.remove(btnConnectDone);
+		    	  absPanel.remove(btnConnectCancel);
+		    	  pickEntityHandler.removeInputAction(ScreenSpaceEventType.LEFT_CLICK());
+		    	  dynamicLineHandler.removeInputAction(ScreenSpaceEventType.MOUSE_MOVE());
+		    	  polylineVerts.clear();
+		      }		      
+		   });
+		  
+		  btnConnectCancel.addClickHandler( new ClickHandler() {
+		      public void onClick(ClickEvent event) {
+				  absPanel.remove(btnConnectDone);
+		    	  absPanel.remove(btnConnectCancel);
+		    	  pickEntityHandler.removeInputAction(ScreenSpaceEventType.LEFT_CLICK());
+		    	  dynamicLineHandler.removeInputAction(ScreenSpaceEventType.MOUSE_MOVE());
+		    	  polylineVerts.clear();
+		    	  myViewer.entities().remove(myViewer.entities().getById(currentLineId));
+		      }		      
+		   });
+		  
+		  absPanel.add(btnConnectPoints, 10, 60);			  
+	  }	  
+	  
+	  //Highlight Entity on mouseover (currently only valid for Point)
+	  PickedObject<Entity> pickedObject = new PickedObject<Entity>();
+	  Entity pickedEntity = new Entity();
+	  ConstantProperty<Color> baseColor = new ConstantProperty<>();
+	  private void highlightOnMouseOver() {
+		  ScreenSpaceEventHandler myHandler = new ScreenSpaceEventHandler(myVPanel.getViewer().scene().canvas());
+		  myHandler.setInputAction(new ScreenSpaceEventHandler.Listener<MouseMoveEvent>() {
+			  @Override
+			  public void function(MouseMoveEvent event) {
+				  
+				  //Determine if a picked entity already existed (from last mouse-move), return it to its original color
+				  if (Cesium.defined(pickedEntity.point)) {
+					 pickedEntity.point.outlineColor = baseColor;
+				  }
+				  
+				  //Pick Entity on mouse-over
+				  try {
+					  pickedObject = (PickedObject<Entity>) myVPanel.getViewer().scene().pick(event.endPosition);	
+					  	
+				  }
+				  catch(ClassCastException e) {
+					  pickedObject = null;
+				  }				  
+				  
+				  //If Cesium found an Entity, determine if it is a point and update the color 
+				  if (Cesium.defined(pickedObject)) {
+					  pickedEntity = pickedObject.id;
+					  if (Cesium.defined(pickedEntity.point)) {
+						  baseColor = (ConstantProperty<Color>) pickedEntity.point.outlineColor;
+						  pickedEntity.point.outlineColor = new ConstantProperty<>(Color.GOLD());
+					  }
+				  }		  
+			  }
+		  }, ScreenSpaceEventType.MOUSE_MOVE());		  
+	  }
+	  
 	  //Show global position in top left corner of viewer on mouse move 
-	  public void showPositionOnMouseOver() {
+	  private void showPositionOnMouseOver() {
 		  HorizontalPanel positionDisplay = new HorizontalPanel();
 		  absPanel.add(positionDisplay, 10, 10);		  
 		  Label lblPosition = new Label();
@@ -316,15 +485,15 @@ public class CesiumDemo implements EntryPoint  {
 	  }	//End of function showPositionOnMouseOver
 	  
 	  //Get position on globe on mouse move
-	  public Cartesian3 getPosition(MouseMoveEvent event) {
+	  private Cartesian3 getPosition(MouseMoveEvent event) {
           Cartesian3 c3Location = myVPanel.getViewer().camera.pickEllipsoid(event.endPosition, myVPanel.getViewer().scene().globe.ellipsoid);
 		  return c3Location;
 	  }	//End of function getPosition(MouseMoveEvent)
 	  
 	  //Get position on globe on mouse click
-	  public Cartesian3 getPosition(MouseClickEvent event) {
+	  private Cartesian3 getPosition(MouseClickEvent event) {
           Cartesian3 c3Location = myVPanel.getViewer().camera.pickEllipsoid(event.position, myVPanel.getViewer().scene().globe.ellipsoid);
 		  return c3Location;
 	  }	//End of function getPosition(MouseClickEvent)
+	  
 }
-
